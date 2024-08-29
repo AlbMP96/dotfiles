08b16efe1ca5ffa94ace27b98b63781aac452ba4
AlbMP96 <92947424+AlbMP96@users.noreply.github.com>
2024-08-29 16:39:52 +0200

    Added battery notification

---
+++ scripts/battery-alert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+while true
+do
+	bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
+	if [ $bat_lvl -lt 15 ]
+	then
+		notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
+		sleep 1200
+	else
+		sleep 120
+	fi
+done
